c12ffc544a0746fe67ab08c86b87a4cf50690d4b
Kavin Bharathi <r.m.kavinbharathi@gmail.com>
2024-05-12 20:00:35 +0530

    some cycle progress

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
+# this is an attempt to crack the k4 of KRYPTOS in Langley, USA
 ALPHAS = 'abcdefghijklmnopqrstuvwxyz'
 
+
+# this is the caesar cipher, just in case it is useful
 def caesar(plaintext, s = 3):
     result = ""
 
@@ -13,7 +16,22 @@ def caesar(plaintext, s = 3):
 
     return result
 
-def vigenere(plaintext, key_stream, key = ""):
+# this is the vigenere cipher, which takes in a plaintext and a key stream, which is used to encrypt the plaintext.
+# the key is optional, if not provided, it will use the entire alphabet
+# the key stream is repeated until it is the same length as the plaintext
+# the plaintext is then encrypted using the key stream
+# for example,
+# 
+# plaintext = "hello"
+# key_stream = "abc"
+# key = "kryptos"
+
+# the key stream will be repeated until it is the same length as the plaintext
+# key_stream = "abca"
+# then the plaintext is encrypted using the key stream, by looking up the values 
+# in the key stream and the plaintext in the vigenere table. The alphabets can be
+# keyed by adding a word infront og the alphabets in the vigenere table.
+def vigenere(plaintext, key_stream, key = "kryptos", encrypt = True):
     if not key == "":
         for i in ALPHAS:
             if i not in key:
@@ -30,14 +48,37 @@ def vigenere(plaintext, key_stream, key = ""):
     
     ciphertext = ""
 
-    for i in range(len(plaintext)):
-        if plaintext[i].isalpha():
-            ciphertext += key[(key.index(key_stream[i]) + key.index(plaintext[i])) % 26]
-        else:
-           ciphertext += plaintext[i]
+    if encrypt:
+        for i in range(len(plaintext)):
+            if plaintext[i].isalpha():
+                ciphertext += key[(key.index(plaintext[i]) + key.index(key_stream[i])) % 26]
+            else:
+                ciphertext += plaintext[i]
+
+    else:
+        for i in range(len(plaintext)):
+            if plaintext[i].isalpha():
+                ciphertext += key[(key.index(plaintext[i]) - key.index(key_stream[i])) % 26]
+            else:
+                ciphertext += plaintext[i]
 
     return ciphertext
 
+
+# this functions finds the key stream used to encrypt the plaintext
+# given the plaintext and the ciphertext.
+def find_keystream(plaintext, ciphertext):
+    res = ""
+    for index, plain_letter in enumerate(plaintext):
+        for letter in ALPHAS:
+            vigenere_text_of_one_letter = vigenere(plaintext=plain_letter, key_stream=letter)
+            if vigenere_text_of_one_letter == ciphertext[index]:
+                res += letter
+
+    print(plaintext, ciphertext, res)
+    return res
+
+# this function is used to find the frequeceny percentage of each letter in the ciphertext
 def getFrequencyData(ciphertext):
     ciphertext = ciphertext.lower()
     letters = set(ciphertext)
@@ -51,11 +92,16 @@ def getFrequencyData(ciphertext):
 
     return data 
 
-# data = [(k, v) for k, v in getFrequencyData(k4).items()]
-# data.sort(key = lambda x: x[1], reverse = True)
 
+cycle_count = 0
+fro = "northeast"
+temp = fro
+key = "dcyygckaz"
+for _ in range(30):
+    print(str(cycle_count).zfill(2), fro)
+    fro = vigenere(fro, key)
+    if fro == temp:
+        print("[cycle found]", fro)
+        break
+    cycle_count += 1
 
-k1 = "EMUFPHZLRFAXYUSDJKZLDKRNSHGNFIVJYQTQUXQBQVYUVLLTREVJYQTMKYRDMFD"
-k4 = "OBKRUOXOGHULBSOLIFBBWFLRVQQPRNGKSSOTWTQSJQSSEKZZWATJKLUDIAWINFBNYPVTTMZFPKWGDKZXTJCDIGKUHUAUEKCAR"
-
-print(vigenere("secretmessage", "hidden", "kryptos"))
